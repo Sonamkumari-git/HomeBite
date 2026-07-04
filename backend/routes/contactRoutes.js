2a50430326636f4803739c83d2274f9b06c334a9
Sonamkumari-git <7258sonam@gmail.com>
2026-07-04 23:03:39 +0530

    Update contactRoutes.js

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { submitContact } = require('../controllers/contactController');
 
 // POST request ko handle karega
-router.post('/contact', submitContact);
+// Dhyan dein: Yahan path ab sirf '/' hai
+router.post('/', submitContact);
 
 module.exports = router;
