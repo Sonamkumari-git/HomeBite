aabb96d3c75a8e0b0ae4b54cc2b191aa001ca4fe
Sonamkumari-git <7258sonam@gmail.com>
2026-07-05 16:50:44 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,6 +1,9 @@
 // backend/config/cloudinary.js
 const cloudinary = require('cloudinary').v2;
 
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME); // Check karne ke liye
+console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
