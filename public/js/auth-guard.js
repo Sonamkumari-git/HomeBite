04e4de2a9407629f53588e6595e3b3bef2a32fa9
Sonamkumari-git <7258sonam@gmail.com>
2026-07-05 16:20:33 +0530

    Create auth-guard.js

---
+++ public/js/auth-guard.js
@@ -0,0 +1,9 @@
+(function () {
+    const token = localStorage.getItem('token');
+    
+    // Agar token nahi mila, toh seedha login page par redirect karo
+    if (!token) {
+        alert("Access Denied! Pehle login karein.");
+        window.location.href = '/login'; // .html lagane ki zaroorat nahi hai
+    }
+})();
