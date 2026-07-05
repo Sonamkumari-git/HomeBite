a208147dfe3c7044479cfe3247aa6d8108cc8e43
Sonamkumari-git <7258sonam@gmail.com>
2026-07-05 17:41:04 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
         type: String, 
         // Default mein wahi image daal di hai jo HTML mein thi
         // Taaki jab naya account bane, toh default blank na dikhe
-        default: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80'
+        
     }
 }, {
     timestamps: true // Kab account bana, uska time save karega
