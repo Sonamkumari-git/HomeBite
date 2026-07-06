5257a617efe65997c89a69b76ae6885168006287
Sonamkumari-git <7258sonam@gmail.com>
2026-07-06 22:31:06 +0530

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { placeOrder, getMyOrders, verifyPayment } = require('../controllers/orderController');
-const { protect } = require('../middlewares/authMiddleware');
+
+// 🔥 YAHAN CHANGE KIYA HAI: 'middlewares' ki jagah 'middleware' (bina 's' ke) kar diya hai
+const { protect } = require('../middleware/authMiddleware'); 
 
 router.post('/place', protect, placeOrder);
-router.post('/verify', protect, verifyPayment); // 🔥 NAYA ROUTE
+router.post('/verify', protect, verifyPayment); 
 router.get('/my-orders', protect, getMyOrders);
 
 module.exports = router;
