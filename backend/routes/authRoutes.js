43d4b6bad5edbc4b02838a1d3937c78a1add75af
Sonamkumari-git <7258sonam@gmail.com>
2026-07-06 17:31:40 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+
+// Controller import karo
 const { registerUser, loginUser } = require('../controllers/authController');
 
-// Sign up API route
-router.post('/register', registerUser);
+// Multer Setup (Memory Storage for Cloudinary)
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-// Login API route
+// 🔥 YAHAN upload.single('profileImage') LAGANA ZAROORI HAI
+// Yeh multer middleware req.body aur req.file ko populate kar dega
+router.post('/register', upload.single('profileImage'), registerUser);
+
+// Login route
 router.post('/login', loginUser);
 
 module.exports = router;
